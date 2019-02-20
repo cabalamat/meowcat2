a27cae4d82cf84c3201ddad9cf17d8a4086e099a
Phil Hunt <cabalamat@gmail.com>
2019-02-20 02:22:43 +0000

    RSS feed

--- app/messlist.py
+++ app/messlist.py
@@ -1,5 +1,7 @@
 # messlist.py = lists of messages
 
+from typing import *
+
 import models
 
 
@@ -15,11 +17,17 @@ class ListFormatter:
     def __init__(self, q):
         self.q = q
 
+    def getMessages(self) -> Iterable[models.Message]:
+        ms = models.Message.find(self.q, 
+             sort=('published', -1),
+             limit=MESS_SHOW)
+        return ms
+        
+        
     def getMessagesH(self) -> str:
         """ Return HTML for the list of messages """
-        ms = models.Message.find(self.q, sort=('published', -1))
         h = ""
-        for m in ms:
+        for m in self.getMessages():
             h += m.viewH() + "<p></p>"
         #//for
         return h
@@ -32,6 +40,9 @@ class ListFormatter:
 
 
     #========== RSS methods
+    
+    def renderRss(self) -> str:
+        """ Return RSS for this feed """
 
 
 #---------------------------------------------------------------------
