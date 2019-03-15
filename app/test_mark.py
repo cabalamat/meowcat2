02b9f68e4e00ed72a2b0ad389b66b3e4b87e7770
Phil Hunt <cabalamat@gmail.com>
2019-03-15 03:06:47 +0000

    test_mark

--- app/test_mark.py
+++ app/test_mark.py
@@ -10,7 +10,24 @@ import mark
 #---------------------------------------------------------------------
 
 class T_mark(lintest.TestCase):  
-    pass
+    
+    def test_normal(self):
+        s = "some text here"
+        h, ts = mark.renderWithTags(s)
+        dpr("----- h:\n%s\n-----end", h)
+        self.assertTrue("some text here" in h)
+        self.assertSame(ts, [], "no tags")
+        
+    def test_tags(self):
+        s = "some text #with #tags and #more #tags"
+        h, ts = mark.renderWithTags(s)
+        dpr("----- h:\n%s\n-----end", h)
+        dpr("ts=%r", ts)
+        #self.assertTrue("some text here" in h)
+        self.assertSame(len(ts), 3, "3 tags")
+        self.assertBool("with" in ts, "'with' tag")
+        self.assertBool("tags" in ts, "'tags' tag")
+        self.assertBool("more" in ts, "'more' tag")
 
     
 #---------------------------------------------------------------------
