e1d6f54ebff4f56d07728f718984363e1267ddf4
Phil Hunt <cabalamat@gmail.com>
2019-09-10 02:02:14 +0100

    added requests

--- app/getrss.py
+++ app/getrss.py
@@ -1,10 +1,16 @@
 # getrss.py = get RSS data
 
+import requests
 
+URL = "http://esr.ibiblio.org/?feed=rss2"
 
+def getRss(u):
+    r = requests.get(u)
+    dpr("status_code=%r", r.status_code)
+    dpr("\n----- r.text=\n%s\n-----(end)", r.text)
 
-
-
+if __name__=='__main__':
+    getRss(URL)
 
 
 #end
